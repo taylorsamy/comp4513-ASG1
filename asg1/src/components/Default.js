b1cebba4ea8ff42a103a16ca6bc762470d260044
Taylor Amy <taylorsamy@users.noreply.github.com>
2023-02-23 19:28:47 -0700

    pulling into localstorage

---
+++ asg1/src/components/Default.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar.js';
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { Link } from 'react-router-dom';
+
+const Default = (props) => {
+    
+  return (
+    <div className="w-full h-[100vh]">
+        <Navbar />      
+
+
+    </div>
+)
+}
+
+export default Default;
