4e17689abb593605655102313567005a66a19487
Nate P <npena535@mtroyal.ca>
2023-02-27 00:09:20 -0700

    added stuff to movie details

--- asg1/src/components/MovieDetails.js
+++ asg1/src/components/MovieDetails.js
@@ -1,23 +1,17 @@
-
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import Navbar from "./Navbar";
-import { AiOutlineLeft } from 'react-icons/ai';
+import { AiOutlineLeft } from "react-icons/ai";
 import MovieDetailsExtra from "./movie-details-components/MovieDetailsExtra";
 import MovieDetailsRatings from "./movie-details-components/MovieDetailsRating";
 import TMDBLogo from "./logos/TMDB.png";
 import IMDBLogo from "./logos/IMDB.png";
 
-
 const MovieDetails = (props) => {
-
-  const img = "https://image.tmdb.org/t/p/w342/55WJCdayHwoXJwRv3prpqscHt2q.jpg";
-  const backdrop =
-    "https:image.tmdb.org/t/p/original/bQ8fRUaitJvi54O2lUT6Ta7FVHK.jpg";
-  const tmdbLink = "https://www.themoviedb.org/movie/25";
-  const imdbLink = "https://www.imdb.com/title/tt0418763";
+  const tmdbLink = `https://www.themoviedb.org/movie/`;
+  const imdbLink = `https://www.imdb.com/title/`;
 
   const [overview, setOverview] = useState(true);
   const [details, setDetails] = useState(false);
@@ -27,20 +21,19 @@ const MovieDetails = (props) => {
     setOverview(true);
     setDetails(false);
     setRatings(false);
-  }
+  };
 
   const handleDetails = () => {
     setDetails(true);
     setOverview(false);
     setRatings(false);
-  }
+  };
 
   const handleRatings = () => {
     setRatings(true);
     setOverview(false);
     setDetails(false);
-  }
-
+  };
 
   const [searchParms, setSearchParms] = useSearchParams();
   const [movie, setMovie] = useState(null);
@@ -52,37 +45,25 @@ const MovieDetails = (props) => {
     const movie = props.movieData.find((m) => m.id == id);
     // set movie
     setMovie(movie);
-    console.log(movie);
   }, [props.movieData, searchParms]);
-
-
-
 
   return (
     <div className="w-full h-[93vh]">
       <Navbar />
-
-      <div className='hidden'>
+      <div className="hidden">
         <AiOutlineLeft />
       </div>
 
-      <img
-        src={backdrop}
-        className="absolute w-full h-[100vh] -z-10 brightness-[0.25] blur-[20px] shadow-none backdrop-blur-sm"
-      ></img>;
-      {console.log(movie)}
-      {movie &&
+      {movie && (
         <div>
           <img
             src={"https:image.tmdb.org/t/p/original" + movie.backdrop}
-            className="absolute w-full h-[93vh] -z-10 brightness-[0.25] blur-[20px] shadow-none backdrop-blur-sm"
+            className="absolute w-full h-[100vh] -z-10 brightness-[0.50] blur-[20px] shadow-none backdrop-blur-sm"
             alt="backdrop"
           ></img>
 
           <div className="grid md:grid-cols-2 min-h-[calc(100vh-70px)]">
-
             <div className="grid place-items-center">
-
               <img
                 className="shadow-xl shadow-gray-900 rounded-md"
                 src={"https://image.tmdb.org/t/p/w500" + movie.poster}
@@ -91,71 +72,78 @@ const MovieDetails = (props) => {
             </div>
             <div className="mx-[50px] py-40 flex flex-col  gap-10 text-white">
               <div className="flex flex-row items-center gap-60">
-                <p className="md:text-5xl sm:text-4xl text-xl font-bold">{movie.title}</p>
+                <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+                  {movie.title}
+                </p>
                 <p className="md:text-2xl">{movie.ratings.average}</p>
               </div>
+              <div className="flex flex-row gap-5">
+                {movie.details.genres.map((genre) => {
+                  return (
+                    <p className="border-solid text-center px-[10px] rounded-lg bg-[#0f7ca7]">
+                      {genre.name}
+                    </p>
+                  );
+                })}
+              </div>
 
-              <div>
-                <p>2005</p>
-
-
-                <div className="flex flex-row content-evenly ">
+              <div className="flex flex-col gap-10">
+                <div className="grid grid-cols-2 ">
                   <p>{movie.release_date.substring(0, 4)}</p>
                   <p>{movie.runtime} mins</p>
-
                 </div>
                 <div className="flex flex-row gap-40">
-                  <p onClick={handleOverview}>Overview</p>
-                  <p onClick={handleDetails}>Details</p>
-                  <p onClick={handleRatings}>Ratings</p>
+                  <button
+                    className="bg-transparent border-b-2 border-gray py-2 mr-3 px-2 leading-tight outline-none hover:border-[#0f7ca7] focus:text-[#52bee9] focus:border-[#0f7ca7]  duration-300"
+                    onClick={handleOverview}
+                  >
+                    Overview
+                  </button>
+                  <button
+                    className="bg-transparent border-b-2 border-gray py-2 mr-3 px-2 leading-tight outline-none hover:border-[#0f7ca7] focus:text-[#52bee9] focus:border-[#0f7ca7]  duration-300"
+                    onClick={handleDetails}
+                  >
+                    Details
+                  </button>
+                  <button
+                    className="bg-transparent border-b-2 border-gray py-2 mr-3 px-2 leading-tight outline-none hover:border-[#0f7ca7] focus:text-[#52bee9] focus:border-[#0f7ca7] duration-300"
+                    onClick={handleRatings}
+                  >
+                    Ratings
+                  </button>
                 </div>
 
-                <div>
-                  {overview === true && details === false && ratings === false && <p>
-                    Jarhead is a film about a US Marine Anthony SwoffordÃ¢â¬Å¡ÃâÃÂ´s
-                    experience in the Gulf War. After putting up with an arduous boot
-                    camp, Swafford and his unit are sent to the Persian Gulf where
-                    they are earger to fight but are forced to stay back from the
-                    action. Meanwhile Swofford gets news of his girlfriend is cheating
-                    on him. Desperately he wants to kill someone and finally put his
-                    training to use.
-                  </p>}
+                <div className="flex flex-col gap-10">
+                  {overview === true &&
+                    details === false &&
+                    ratings === false && <p>{movie.details.overview}</p>}
 
-                  {details === true && overview === false && ratings === false &&
-                    <MovieDetailsExtra />}
+                  {details === true &&
+                    overview === false &&
+                    ratings === false && <MovieDetailsExtra movie={movie} />}
 
-                  {ratings === true && overview === false && details === false &&
-                    <MovieDetailsRatings />}
+                  {ratings === true &&
+                    overview === false &&
+                    details === false && <MovieDetailsRatings movie={movie} />}
 
-
-                  <div className="flex flex-row justify-between">
-                    <p>
-                      {movie.details.overview}
-                    </p>
-
-                  </div>
                   <div className="flex flex-col gap-5">
-                    <div className="flex flex-row gap-10">
-                      <p>Genre</p>
-                      <p>Drama, War</p>
-                    </div>
-
                     <div className=" overflow-auto flex flex-row gap-10 items-center mt-5">
-                      <a href={imdbLink} className='w-[75px]' rel='noreferrer' target='_blank'><img src={IMDBLogo} alt="IMDB logo" /></a>
-                      <a href={tmdbLink} className='w-[75px]' rel='noreferrer' target='_blank'><img src={TMDBLogo} alt="TMDB logo" /></a>
-
-                      <div className="flex flex-row gap-10">
-                        <p>Genre</p>
-                        <p>Drama, War</p>
-                      </div>
-                      <div className=" overflow-auto flex flex-row gap-10">
-                        <a href={"https://www.imdb.com/title/" + movie.imdb_id} target="_blank" rel="noreferrer">
-                          TMDB Link
-                        </a>
-                        <p>TMDB link : {"https://www.themoviedb.org/movie/" + movie.tmdb_id}</p>
-                        <p>IMDB link : {"https://www.imdb.com/title/" + movie.imdb_id}</p>
-
-                      </div>
+                      <a
+                        href={imdbLink + movie.imdb_id}
+                        className="w-[75px]"
+                        rel="noreferrer"
+                        target="_blank"
+                      >
+                        <img src={IMDBLogo} alt="IMDB logo" />
+                      </a>
+                      <a
+                        href={tmdbLink + movie.tmdb_id}
+                        className="w-[75px]"
+                        rel="noreferrer"
+                        target="_blank"
+                      >
+                        <img src={TMDBLogo} alt="TMDB logo" />
+                      </a>
                     </div>
                   </div>
                 </div>
@@ -163,11 +151,9 @@ const MovieDetails = (props) => {
             </div>
           </div>
         </div>
-        }
-
-
-        </div>
-          );
+      )}
+    </div>
+  );
 };
 
-      export default MovieDetails;
+export default MovieDetails;
--- asg1/src/components/movie-details-components/MovieDetailsExtra.js
+++ asg1/src/components/movie-details-components/MovieDetailsExtra.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const MovieDetailsExtra = (props) => {
-    return(
-        <div className='flex flex-col gap-5'>
-            <div className='grid grid-cols-2'>
-                <p>Tagline</p>
-                <p className='italic'>"Welcome to the suck."</p>
-            </div>
-            <div className='grid grid-cols-2'>
-                <p>Revenue</p>
-                <p>$96,889,998</p>
-            </div>
-            <div className='grid grid-cols-2'>
-                <p>Release Date</p>
-                <p>2005-11-04</p>
-            </div>
-        </div>
-    )
-}
+  // Reference for converting INT values to Currency Format
+  // https://www.freecodecamp.org/news/how-to-format-number-as-currency-in-javascript-one-line-of-code/
+  const currency = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  const movieRev = currency.format(props.movie.revenue);
+
+  return (
+    <div className="flex flex-col gap-5 mr-10">
+      <div className="grid grid-cols-2">
+        <p>Tagline</p>
+        <p className="italic">"{props.movie.tagline}"</p>
+      </div>
+      <div className="grid grid-cols-2">
+        <p>Revenue</p>
+        <p>{movieRev}</p>
+      </div>
+      <div className="grid grid-cols-2">
+        <p>Release Date</p>
+        <p>{props.movie.release_date}</p>
+      </div>
+    </div>
+  );
+};
 
 export default MovieDetailsExtra;
