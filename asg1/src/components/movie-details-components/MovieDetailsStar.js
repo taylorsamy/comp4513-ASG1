d30648ddbf4b253f77fc28597f17cac1542e842a
npena535 <npena535@mtroyal.ca>
2023-02-28 12:57:22 -0700

    stars

--- asg1/src/components/movie-details-components/MovieDetailsStar.js
+++ asg1/src/components/movie-details-components/MovieDetailsStar.js
@@ -2,34 +2,35 @@ import React from "react";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 
 const MovieDetailsStar = (props) => {
-
   const roundedNumber = Math.floor(Math.ceil(props.ratings * 2) / 2);
 
   const StarList = () => {
-    
-    const wholeNum = roundedNumber / 2;
+    const wholeNum = Math.trunc(roundedNumber / 2);
     const decimalNum = roundedNumber % 2;
     let stars = [];
-    
-    for(let i = 0; i < wholeNum; i++){
+    console.log(roundedNumber, wholeNum, decimalNum);
+    for (let i = 0; i <= 5; i++) {
+      if (i < wholeNum) {
         stars.push(<BsStarFill />);
+      }
+
+      if (i > wholeNum) {
+        if (i === wholeNum) {
+          if (decimalNum >= 0.9) {
+            stars.push(<BsStarHalf />);
+          } else {
+            stars.push(<BsStar />);
+          }
+        } else{
+          stars.push(<BsStar />)
+        }
+      }
     }
 
-    if(decimalNum >= 1){
-        stars.push(<BsStarHalf />)
-    } else {
-        stars.push(<BsStar />)
-    }
-    
-    console.log(stars);
     return stars;
-  }
+  };
 
-  return (
-    <div>
-      <StarList />
-    </div>
-  );
+  return <div>{<StarList />}</div>;
 };
 
 export default MovieDetailsStar;
